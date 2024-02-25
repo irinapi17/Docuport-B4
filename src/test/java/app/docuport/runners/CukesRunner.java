f95474c1d676d19fa33a5d14d922d6e8a20ffa39
primarypc <gwwlax@gmail.com>
2024-02-25 07:53:03 -0500

    commit

--- src/test/java/app/docuport/runners/CukesRunner.java
+++ src/test/java/app/docuport/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
                 "rerun:target/rerun.txt"},
         features = "src/test/resources/features/",
         glue = "app/docuport/step_definitions/",
-        tags = "@stockData",
+        tags = "",
         dryRun = false
 )
 public class CukesRunner {
