9e35a4b9e0440797added85c251af75ccf76e52d
primarypc <gwwlax@gmail.com>
2024-02-25 23:15:20 -0500

    commit

--- src/test/java/app/docuport/runners/CukesRunner.java
+++ src/test/java/app/docuport/runners/CukesRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"json:target/cucumber.json",
-                "html:target/cucumber/report.html",
+                //"html:target/cucumber/report.html",
                 "junit:target/junit/junit-report.xml",
                 "rerun:target/rerun.txt"},
         features = "src/test/resources/features/",
